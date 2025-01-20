99a550ae37a213d8f47aa16fa6cf96730bb494f9
Howard Lam <lampuiho@msn.com>
2025-01-20 23:34:20 +0000

    Add files via upload

---
+++ MovablePlatform_0.py
@@ -0,0 +1,17 @@
+﻿#import the joy of programming python module pyjop
+from pyjop import *
+#connect to the current SimEnv
+SimEnv.connect()
+
+platform = MovablePlatform.first()
+#set_target_location to move platform above dumpster
+#sleep until arrived
+#rotate platform to drop barrel
+platform.set_target_location(0,0,12)
+sleep(3)
+platform.set_target_location(-5,0,12)
+sleep(3)
+platform.set_target_rotation(50,0,0)
+
+#cleanup close code
+SimEnv.disconnect()
---
+++ Selection_0.py
@@ -0,0 +1,15 @@
+﻿#import the joy of programming python module pyjop
+from pyjop import *
+#connect to the current SimEnv
+SimEnv.connect()
+
+# In this tutorial you need to combine what you learned in the previous two tutorials:
+# iterate all conveyor belts, check each belt if it is transporting something or not and then set the speed > 0 if (and only if) that is True
+forward = 1
+for belt in ConveyorBelt.find_all():
+    if belt.get_is_transporting():
+        belt.set_target_speed(10*forward)
+        forward *= -1
+
+#cleanup close code
+SimEnv.disconnect()
